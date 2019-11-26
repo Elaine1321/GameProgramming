f277869bc2c997d931e5f8d645f3516ba92a0b08
Elaine1321 <56021255+Elaine1321@users.noreply.github.com>
2019-11-26 11:17:43 +0100

    laser
    update

---
+++ LaserDefender/Assets/Script/EnemyPath.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+
+
+{
+
+[SerializeField] List<Transform> waypoints;
+
+[SerializeField] float enemySpeed = 2f;
+
+int waypointsIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointsIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    private void EnemyMove()
+    {
+        if(waypointsIndex < waypoints.Count)
+        {
+            //set targetPosition to the next waypoint
+            Vector3 TargetPosition= waypoints[waypointsIndex].transform.position;
+            TargetPosition.z=0f;
+
+            float movmentThisFrame = enemySpeed * Time.deltaTime;
+
+            transform.position=Vector3.MoveTowards(transform.position,TargetPosition,movmentThisFrame);
+
+            if(transform.position == TargetPosition)
+            {
+                waypointsIndex++;
+            }
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
